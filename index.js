a96ac032276a4aaaad0c5f6a8f12fafbe3652319
the-pot-bit <kameebipin@gmail.com>
2026-07-31 18:51:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-# my-oast-pot-server
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // This logs every hit inside your Render web dashboard panel
     console.log(`[!] EXFILTRATION HIT DETECTED!`);
     console.log(`Method: ${req.method}`);
     console.log(`URL/Payload: ${req.url}`);
     
-    // Bypasses browser security controls (CORS)
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('OK');
